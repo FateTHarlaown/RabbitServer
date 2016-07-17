930d2587f04953eedd74767271a436ecd52296a3
FateTHarlaown <FateTHarlaown@github.com>
2016-07-17 19:20:15 +0800

    basic timer class

---
+++ net/Timer.cpp
@@ -0,0 +1,39 @@
+#include "Timer.h"
+#include <stdio.h>
+
+using namespace Rabbit;
+using namespace Rabbit::net;
+using namespace Rabbit::base;
+
+Timer::Timer(TimerCallBack callBack, Rabbit::base::Timestamp expiration, bool repeat, int interval) : callBack_(callBack), expiration_(expiration), repeat_(repeat), interval_(interval)
+{
+
+}
+
+void Timer::restart()
+{
+	if(repeat_)
+	{
+		expiration_ = Timestamp(Timestamp::nowAfter(interval_));
+	} 
+	else
+	{
+		printf("this timer can not restart, ignore it\n");
+	}
+}
+
+void Timer::run()
+{
+	callBack_();
+}
+
+bool Timer::isRepeated()
+{
+	return repeat_;
+}
+
+Timestamp Timer::getExpiration()
+{
+	return expiration_;
+}
+
---
+++ net/Timer.h
@@ -0,0 +1,31 @@
+#ifndef RABBIT_NET_TIMER
+#define RABBIT_NET_TIMER
+
+#include <boost/function.hpp>
+#include <boost/noncopyable.hpp>
+#include "../base/Timestamp.h"
+
+namespace Rabbit{
+namespace net{
+class Timer:boost::noncopyable
+{
+public:
+	typedef boost::function<void ()> TimerCallBack;
+	Timer(TimerCallBack callBack, Rabbit::base::Timestamp expiration, bool repeat, int interval);
+	//call the callBack function when the timer expired
+	void run();
+	bool isRepeated();
+	void restart();
+	Rabbit::base::Timestamp getExpiration();
+
+private:
+	TimerCallBack callBack_;
+	Rabbit::base::Timestamp expiration_;
+	const bool repeat_;
+	const int interval_;
+};
+
+}
+}
+
+#endif
